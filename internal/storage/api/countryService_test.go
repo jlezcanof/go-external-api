18762954a9cd33cc28d5f22cba52a8b97988cf73
Jose Manuel Lezcano Fresno <reymidas76@yahoo.es>
2022-11-13 11:46:52 +0100

    include data provider for test

--- internal/storage/api/countryService_test.go
+++ internal/storage/api/countryService_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"errors"
 	"testing"
 )
 
@@ -14,11 +15,32 @@ import (
 
 func TestGetCountries(t *testing.T) {
 
-	service := NewCountryService()
-	var url = "https://restcountries.com/v3.1/all"
-
-	_, err := service.GetCountries(url)
-	if err != nil {
-		t.Fatalf("got an error %v", err)
+	tests := map[string]struct {
+		url string
+		err error
+	}{
+		"all the countries":    {url: "https://restcountries.com/v3.1/all", err: nil},
+		"obtain country spain": {url: "https://restcountries.com/v3.1/name/spain?fullText=false", err: nil},
+		"not exists country":   {url: "https://restcountries.com/v3.1/name/porta?fullText=false", err: errors.New("error")},
 	}
+
+	service := NewCountryService()
+
+	for name, tc := range tests {
+
+		t.Run(name, func(t *testing.T) {
+			_, err := service.GetCountries(tc.url) //b, err
+			if err != nil && tc.err == nil {
+				t.Fatalf("not expected any errors and got nil %v", err)
+			}
+
+			if err == nil && tc.err != nil {
+				t.Error("expected and arror got nil")
+			}
+
+			//if tc.want != nil
+
+		})
+	}
+
 }
